38f4d4fb950145b550dbe66b95b6ef52825d80d2
peeryxp <peeryxp@gmail.com>
2025-01-22 07:34:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Domain Parked</title>
+    <title>Domain Parked - <?php echo $_SERVER['SERVER_NAME']; ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
