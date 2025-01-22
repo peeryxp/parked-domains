7e4f4fb31359e0166a6d5f51897d6333aa694564
peeryxp <peeryxp@gmail.com>
2025-01-22 05:16:22 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Domain Parked</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5MB79S7BV2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-5MB79S7BV2');
+    </script>
 </head>
 <body>
     <div class="container text-center my-5">
