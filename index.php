3ef1c2a208860f9ef8ffa980042c7f16de28a9b8
Paul Peery <peeryxp@gmail.com>
2025-01-23 06:34:48 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Domain Parked</title>
+    <title>Domain Parked - Paul Peery</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/styles.css"> <!-- Custom CSS -->
     <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
